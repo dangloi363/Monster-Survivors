ea96a3d4cd076b778e9deb5d99afd9d7d5641e87
Khanh Nguyen <132930424+khanhnyuen@users.noreply.github.com>
2024-01-03 02:30:52 +0700

    add audioManager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource musicSource;
+
+    public AudioClip background;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+        DontDestroyOnLoad(transform.gameObject);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
