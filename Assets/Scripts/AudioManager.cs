21f4c9995877aeaf7afde73bcf1cad979126aca9
Khanh Nguyen <132930424+khanhnyuen@users.noreply.github.com>
2024-01-10 23:28:56 +0700

    update stop music when scene Game

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,25 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] AudioSource musicSource;
-
+    [SerializeField] AudioSource ingameSource;
     public AudioClip background;
-
+    public AudioClip ingamebg;
+    public static AudioManager instance;
     // Start is called before the first frame update
     void Start()
     {
+       
+    }
+
+    void Awake()
+    {
         musicSource.clip = background;
+        ingameSource.clip = ingamebg;
         musicSource.Play();
-        DontDestroyOnLoad(transform.gameObject);
-        
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (SceneManager.GetActiveScene().name == "Game")
+        {
+            musicSource.Stop();
+        }
     }
 }
