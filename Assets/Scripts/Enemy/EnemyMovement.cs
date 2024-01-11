582b342540f552238779a0913d07b1e930bdaff1
dangloi363 <114212494+dangloi363@users.noreply.github.com>
2024-01-11 16:27:57 +0700

    add 4 enemy:BlueGhost,Bone,RedDemon,BigBoss

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -6,15 +6,37 @@ public class EnemyMovement : MonoBehaviour
 {
     EnemyStats enemy;
     Transform player;
-
+    SpriteRenderer sr;
+   
     void Start()
     {
         enemy = GetComponent<EnemyStats>(); 
         player = FindObjectOfType<PlayerMovement>().transform;
+        sr = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position,player.transform.position, enemy.currentMoveSpeed*Time.deltaTime);
+        
+            
+            SpriteDirectionChecker();
+        
+        
+    }
+    void SpriteDirectionChecker()
+    {
+        if (enemy.transform.position.x > player.position.x)
+        {
+            sr.flipX = true;
+        }
+        else
+        {
+            sr.flipX = false;
+        }
     }
 }
+
+
+
+    
